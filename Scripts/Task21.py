914cc6e1b7a4b928c87f78e8355b3cce64c06c04
Vejni <vmarcell000@gmail.com>
2019-12-16 19:25:06 +0000

    Reworking Task21 and moving files

--- Scripts/Task21.py
+++ Scripts/Task21.py
@@ -1,18 +1,25 @@
 from bisect import bisect_left
 import sys
 
+nodes = []  # Array to store created nodes
+
 
 # Node class definition
 class Node(object):
+
+    @staticmethod
+    def getroot():
+        return nodes[-1]
+
+    @staticmethod
+    def getnodes():
+        return nodes
 
     def __init__(self, value):
         self.value = value  # Splitting value
         self.left = None    # left child
         self.right = None   # right child
-        nodes.append(self)  # automatically append to nodes array
-
-    def getval(self):
-        return self.value
+        nodes.append(self)  # automatically append to nodes arrays
 
     # Function to get the value of its rightmost child,
     # will be called on the left child, to get splitting value
@@ -23,13 +30,13 @@ class Node(object):
             return self.right.getrightmost()
 
     # Function to print the LEAF nodes of a node
-    def getleafs(self):
+    def getleafs(self, print_res=False):
         if self.left:
             self.left.getleafs()
         if self.right:
             self.right.getleafs()
-        else:
-            print(self.getval(), end=" ")
+        elif PRINT_RES:
+            print(self.value, end=" ")
 
 
 # Storing numbers in order, like in Task22
@@ -46,7 +53,7 @@ def create_next_level(listA):
     # Check if the number of nodes on the level is odd,
     # if it is store the last element, and leave it out of the for loop
     if len(listA) % 2 == 1:
-        last_element = listA[len(listA)-1]
+        last_element = listA[-1]
         for i in range(0, len(listA)-2, 2):       # For every pair of nodes
             next_node = listA[i].getrightmost()  # Get the splitting value
             next_node = Node(next_node)           # Make it a node
@@ -116,52 +123,58 @@ def create_next_level_reverse(listA):
 
 # Find splitting node, it returns the root of the query
 def find_split_node(root, start_point, end_point):
-    X = root.getval()
-    while (root.right is not None) and (end_point <= X or start_point > X):
-        if end_point <= X:
+    x = root.value
+    while (root.right is not None) and (end_point <= x or start_point > x):
+        if end_point <= x:
             root = root.left
         else:
             root = root.right
-        X = root.getval()
+        x = root.value
     return root
 
 
 # Query, implemented as given,
 # it prints the numbers between the end_points, returns nothing
-def one_D_range_query(root, start_point, end_point):
+def query(root, start_point, end_point, print_res=False):
+    global PRINT_RES
+    PRINT_RES = print_res
     v_split = find_split_node(root, start_point, end_point)
     # If v_split is a leaf, check if it should be returned
     if v_split.right is None:
-        if v_split.getval() >= start_point and v_split.getval() <= end_point:
-            print(v_split.getval(), end=" ")
+        if (v_split.value >= start_point
+                and v_split.value <= end_point
+                and PRINT_RES):
+            print(v_split.value, end=" ")
             return
     else:
         # If not, look at left subtree
         v = v_split.left
         while v.left is not None:
-            if start_point <= v.getval():
-                v.right.getleafs()
+            if start_point <= v.value:
+                v.right.getleafs(print_res)
                 v = v.left
             else:
                 v = v.right
         if (v is not None
-                and (v.getval() >= start_point)
-                and (v.getval() <= end_point)):
-            print(v.getval(), end=" ")
+                and (v.value >= start_point)
+                and (v.value <= end_point)
+                and PRINT_RES):
+            print(v.value, end=" ")
 
         # Then at right subtree
         v = v_split.right
         while v.left is not None:
-            if end_point >= v.getval():
+            if end_point >= v.value:
                 if v.left:
-                    v.left.getleafs()
+                    v.left.getleafs(print_res)
                 v = v.right
             else:
                 v = v.left
         if (v is not None
-                and (v.getval() >= start_point)
-                and (v.getval() <= end_point)):
-            print(v.getval(), end=" ")
+                and (v.value >= start_point)
+                and (v.value <= end_point)
+                and PRINT_RES):
+            print(v.value, end=" ")
 
         return
 
@@ -169,7 +182,6 @@ def one_D_range_query(root, start_point, end_point):
 if __name__ == "__main__":
     # Initialisation
     list_of_integers = []  # Array for the input numbers
-    nodes = []  # Array to store created nodes
 
     # Read the first line
     first_line = sys.stdin.readline()
@@ -192,10 +204,10 @@ if __name__ == "__main__":
     create_next_level(nodes)
     # Finaly, the last created node,
     # will be the root of the whole tree, so we store that
-    root = nodes[len(nodes)-1]
+    root = Node.getroot()
 
     # Read queries
     for _ in range(no_queries):
         line = sys.stdin.readline()
         input_queries = [int(i) for i in line.strip().split()]
-        one_D_range_query(root, input_queries[0], input_queries[1])
+        query(root, input_queries[0], input_queries[1], True)
